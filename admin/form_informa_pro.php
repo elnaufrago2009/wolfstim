0a33aabe0f24ef344723071ce6d23c75e120f297
moises <elnaufrago2009@gmail.com>
2018-08-07 20:42:27 -0500

    base

--- admin/form_informa_pro.php
+++ admin/form_informa_pro.php
@@ -1,5 +1,26 @@
 <?php 
 
-	echo 'true';
+
+	/*
+	 *	Archivo que procesa si va salir el formulario de envio de pago
+	*/
+
+
+
+  //conexion importa
+  include '../conn.php';
+
+
+  //datos recibidos por get
+  $userid 		=		$_GET['userid'];  
+
+
+
+  // consulta si hay 
+  $informaPendientes = $conn->query("SELECT * FROM user_pack where user_id=$user_id and estado=0")->fetch_array(MYSQLI_ASSOC);
+
+  
+
+  echo 'true';
 
 ?>
